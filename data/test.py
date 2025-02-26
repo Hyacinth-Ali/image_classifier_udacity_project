31d19172fb8fe7cb06963ed19962c0e1d3a699e7
Hyacinth Ali <hyacinth.ali@mail.mcgill.ca>
2025-02-26 05:50:58 +0000

    Innitial Commmit

---
+++ data/test.py
@@ -0,0 +1,22 @@
+from os import listdir
+
+image_files = listdir("/workspace/cd0184/9664b117-d773-4799-b6a3-d4640ed70218/data")
+
+# for image_file in image_files:
+#     print(type(image_file))
+
+# print(image_files[0] + "/" + image_files[1])
+
+# x = 1
+
+# test_v = 1 if x == 0 else 0
+
+# print(test_v)
+
+# dic_result = {1:3, 3:4, 5:8}
+# for k, v in dic_result.items():
+    # print(v)
+
+
+check = 0 and 3
+print(check)
